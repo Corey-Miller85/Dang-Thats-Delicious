b66a032b7f5b63484ea72467cad4fd8c3bed2753
Corey Miller <coreyMiller353@gmail.com>
2019-10-31 13:49:30 -0400

    stores now populate /stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,18 +1,28 @@
-const mongoose = require('mongoose');
-const Store = mongoose.model('Store');
+const mongoose = require("mongoose");
+const Store = mongoose.model("Store");
 
 exports.homePage = (req, res) => {
-    res.render('index');
+	res.render("index");
 };
 
 exports.addStore = (req, res) => {
-    res.render('editStore', {
-        title: 'Add Store'
-    })
-}
+	res.render("editStore", {
+		title: "Add Store"
+	});
+};
 exports.createStore = async (req, res) => {
-    const store = new Store(req.body);
-    await store.save();
-    res.redirect('/');
-}
+	const store = await (new Store(req.body)).save();
+	req.flash(
+		"success",
+		`Successfully created ${store.name} care to leave a review?`
+	);
+	res.redirect(`/store/${store.slug}`);
+};
 
+
+exports.getStores = async (req, res) => {
+    // 1. Query the database for a list of all stores
+    const stores = await Store.find();
+    console.log(stores)
+    res.render('stores', { "title": "Stores", stores })
+}
